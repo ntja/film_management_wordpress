9ae665b08f0e11fd62a472181ceb7c0458fcd9b2
ntja <jephte.ndongmou@gmail.com>
2017-12-03 02:22:27 +0100

    Added shortcode pluging and DB dumb

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -3,6 +3,8 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+// create new post type (films)
 function create_post_type() {
     register_post_type( 'films',
         array(
@@ -15,10 +17,13 @@ function create_post_type() {
         'rewrite' => array('slug' => 'films')
         )
     );
+	flush_rewrite_rules();
 }
  
 add_action( 'init', 'create_post_type' );
 
+
+// Add genre taxonomy to film post
 function genre_taxonomy() {
    register_taxonomy(
 		'genre',
@@ -33,6 +38,7 @@ function genre_taxonomy() {
 }
 add_action( 'init', 'genre_taxonomy' );
 
+//add country taxonomy to film post
 function country_taxonomy() {
    register_taxonomy(
 		'country',
@@ -47,6 +53,7 @@ function country_taxonomy() {
 }
 add_action( 'init', 'country_taxonomy' );
 
+//add actor taxonomy to film post
 function actor_taxonomy() {
    register_taxonomy(
 		'actor',
@@ -61,6 +68,7 @@ function actor_taxonomy() {
 }
 add_action( 'init', 'actor_taxonomy' );
 
+//Add year taxonomy to film post
 function year_taxonomy() {
    register_taxonomy(
 		'year',
@@ -74,3 +82,70 @@ function year_taxonomy() {
 	);
 }
 add_action( 'init', 'year_taxonomy' );
+
+// Add custom field (rekease_date and ticket_price) to the_content()
+function custom_post_type_filed($content){
+	global $post;	
+	//var_dump($post->post_type); die();
+	if($post->post_type == 'films'){		
+		$date = new dateTime(get_post_meta($post->ID, 'release_date', true));
+		//var_dump($date->format('m-d-Y')); die();
+		$content .= '<br><br>Release Date : ' . $date->format('m-d-Y');
+		$content .= '<br>Ticket Price : ' . get_post_meta($post->ID, 'ticket_price', true);
+		// country taxonomy
+		$terms = get_the_terms( $post->ID, 'country' ); 				
+		if($terms){
+			$content .= '<br> Country : ';
+			foreach($terms as $term) {
+			  $content .= $term->name;
+				if ($term !== end($terms)){
+					$content .= ' | ';
+				}
+			}
+		}		
+		// genre taxonomy
+		$terms = get_the_terms( $post->ID, 'genre' ); 				
+		if($terms){
+			$content .= '<br> Genre : ';
+			foreach($terms as $term) {
+				$content .= $term->name;
+				if ($term !== end($terms)){
+					$content .= ' | ';
+				}			  
+			}
+		}
+	}	
+	return $content;
+}
+add_filter('the_content', 'custom_post_type_filed', 0);
+
+add_shortcode('last_5_films', 'film_shortcode_query');
+
+
+// show last 5 films on film widget
+function film_shortcode_query(){
+  $args = array( 'post_type' => 'films', 'posts_per_page' => 5 );
+  $posts = new WP_Query($args);
+  $output = '';
+    if ($posts->have_posts()){
+		while ($posts->have_posts()):
+            $posts->the_post();
+            $out .= '<div class="film_box">
+                <h4><a href="'.get_permalink().'" title="' . get_the_title() . '">'.get_the_title() .'</a></h4>';                
+            $out .='</div>';    
+		endwhile;  
+	}        
+
+  wp_reset_query();
+  return $out;
+}
+
+add_filter( 'pre_get_posts', 'my_get_films' );
+
+// Showing films post type on home page
+function my_get_films( $query ) {
+	if ( is_home() && $query->is_main_query() ){
+		$query->set( 'post_type', array( 'films'));
+	}		
+	return $query;
+}
--- wp-content/themes/unite-child/page-templates/single-film.php
+++ wp-content/themes/unite-child/page-templates/single-film.php
@@ -2,40 +2,35 @@
 Template Name: film-template
 Template Post Type: films
 */
-get_header(); ?>
+get_header(); 
+?>
 
 	<div id="primary" class="content-area col-sm-12 col-md-8 <?php echo of_get_option( 'site_layout' ); ?>">
-		<main id="main" class="site-main" role="main">
-					
+		<main id="main" class="site-main" role="main">					
 		<?php 
 			/**/
 			$args = array( 'post_type' => 'films', 'posts_per_page' => 10 );
 			$loop = new WP_Query( $args );
+			//die('here');
 			while ( $loop->have_posts() ) : $loop->the_post();
 				//the_title();
-				//the_content();
-				get_template_part( 'content', 'single' );
-				//date('m/d/Y', strtotime(the_field( 'release_date' )));
-				//the_field( 'ticket_price' );
+				//the_content();				
+				//get_post_format()
+				get_template_part( 'content', 'film' );
 				//the_title();
 				echo '<div class="entry-content">';
-				//the_excerpt();
-				echo '</div>';
-				
-				//echo '<div class="entry-content">';
+				//the_content();
+				//echo 'Release Date :';
 				//date('m/d/Y', strtotime(the_field( 'release_date' )));
+				//echo '<br> Ticket Price :';
 				//the_field( 'ticket_price' );
-				//echo '</div>';
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() ) :
-					comments_template();
-				endif;
+				echo '</div>';					
 			endwhile;
-			
+			wp_reset_postdata();
 		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php //get_sidebar(); ?>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
